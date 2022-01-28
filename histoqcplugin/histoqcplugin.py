006612281e97432403f6d9704b9a90ab6920ebff
VarunVerma7 <varunverma855@gmail.com>
2022-01-28 06:16:42 -0500

    Working instance

--- histoqcplugin/histoqcplugin.py
+++ histoqcplugin/histoqcplugin.py
@@ -3,14 +3,81 @@ import girder_client
 import os
 import tempfile
 import subprocess
-import imageio
 import numpy as np
+import imageio
+
+
+def descend_folder(gc, folder_id, verbose=True):
+    # Recursively descend into folders, finding slides and other folders
+
+    # recursively call on subfolders
+    for folder in gc.listFolder(
+        parentId=folder_id, parentFolderType="folder", limit=None
+    ):
+        descend_folder(gc, folder["_id"], verbose=True)
+
+    # get list of slides in current folder
+    for slide in gc.listItem(folderId=folder_id, limit=None):
+        query_slide(gc, slide["_id"], verbose)
+
+    return
+
+
+def query_slide(gc, slide_id, parentId, verbose=True):
+
+    # Download folder or download item?
+
+    supported_extensions = [".svs", ".tif", ".tiff" ".svslide", ".scn", ".vmu"]
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        gc.downloadItem(slide_id, tmpdir)
+
+        print("LISTING directory after download ", os.listdir(tmpdir))
+
+        print("ABSOLUTE PATH AFTER DOWNLOAD ", os.path.abspath(tmpdir))
+
+        item = gc.getItem(slide_id)
+        item_name = item.get("name", "None")
+        gc.downloadItem(slide_id, tmpdir)
+
+        # path is histoqc/tmpdir/itemname
+
+        _, extension = os.path.splitext(item_name)
+
+        print(f"EXTENSION IS {extension} ITEM NAME IS {item_name}")
+
+        if extension in supported_extensions:
+            print("Extension found in supported extensions")
+            os.chdir("../HistoQC")
+
+            print(f"THIS IS THE TMP DIR {tmpdir}")
+            input_directory = f"{tmpdir}/{item_name}"
+
+            print(f"{input_directory}")
+
+            subprocess.call(
+                f"python3 -m histoqc {input_directory} -o {tmpdir}/outputs --force",
+                shell=True,
+            )
+            metadata_response_dict = process_image(tmpdir, item_name)
+
+            print(f"Meta Data response dictionary {metadata_response_dict}")
+
+            gc.addMetadataToItem(slide_id, metadata_response_dict)
+            gc.upload(f"..{tmpdir}/{slide_id}", parentId)
+
+            print("Uploading completed!")
+
+        else:
+            print(f"File type not supported with item name of {item_name}")
+
+    return
 
 
 def process_image(tmp_directory, item_name):
     try:
         final_mask = imageio.imread(
-            f"../histoqc{tmp_directory}/outputs//item_name}/{item_name}_mask_use.png"
+            f"{tmp_directory}/outputs/{item_name}/{item_name}_mask_use.png"
         )
 
         print("Successfully read in final_mask")
@@ -34,71 +101,6 @@ def process_image(tmp_directory, item_name):
         return {"histoqc_metadata": {"process": "failed"}}
 
     return final_response
-
-
-def descend_folder(gc, folder_id, verbose=True):
-    # Recursively descend into folders, finding slides and other folders
-
-    # recursively call on subfolders
-    for folder in gc.listFolder(
-        parentId=folder_id, parentFolderType="folder", limit=None
-    ):
-        descend_folder(gc, folder["_id"], verbose=True)
-
-    # get list of slides in current folder
-    for slide in gc.listItem(folderId=folder_id, limit=None):
-        query_slide(gc, slide["_id"], parentId=folder_id, verbose=True)
-
-    return
-
-
-def query_slide(gc, slide_id, parentId, verbose=True):
-
-    # Download folder or download item?
-
-    supported_extensions = [".svs", ".tif", ".tiff" ".svslide", ".scn", ".vmu"]
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        gc.downloadItem(slide_id, tmpdir)
-
-        item = gc.getItem(slide_id)
-        item_name = item.get("name", "None")
-        gc.downloadItem(slide_id, tmpdir)
-
-        # path is histoqc/tmpdir/itemname
-
-        _, extension = os.path.splitext(item_name)
-
-        print(f"EXTENSION IS {extension} ITEM NAME IS {item_name}")
-
-        if extension in supported_extensions:
-            print("Extension found in supported extensions")
-            os.chdir("../HistoQC2")
-
-            print(f"THIS IS THE TMP DIR {tmpdir}")
-            input_directory = f"../histoqc{tmpdir}/{item_name}"
-
-            print(f"{input_directory}")
-
-            print(f"Listing input directory {os.listdir(input_directory)}")
-
-            subprocess.call(
-                f"python3 -m histoqc {input_directory} -o ../histoqc{tmpdir}/outputs --force",
-                shell=True,
-            )
-            metadata_response_dict = process_image(tmpdir, item_name)
-
-            print(f"Meta Data response dictionary {metadata_response_dict}")
-
-            gc.addMetadataToItem(slide_id, metadata_response_dict)
-            gc.upload(f"../histoqc{tmpdir}/{slide_id}", parentId)
-
-            print("Uploading completed!")
-
-        else:
-            print(f"File type not supported with item name of {item_name}")
-
-    return
 
 
 def parse_dir_input(directory):
@@ -136,4 +138,3 @@ def main(args):
 
 if __name__ == "__main__":
     main(CLIArgumentParser().parse_args())
-
